1b553fbee80fb4061d358bb4fc694e92b5c57ba0
Jonas Rivadeneyra <aznaranjonas@gmail.com>
2025-07-01 21:34:09 -0500

    1 jul

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -24,7 +24,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.muestraElMenu();
-
+//        EjemploStreams ejemploStreams = new EjemploStreams();
+//        ejemploStreams.muestraEjemplo();
 
     }
 
--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -3,12 +3,15 @@ package com.aluracursos.screenmatch.principal;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.model.Episodio;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
@@ -52,20 +55,27 @@ public class Principal {
         temporadas.forEach(t-> t.episodios().forEach(e->
                 System.out.println(e.titulo())));
 
+        //convertir todas las informaciones a una lista del tipo datios episodio
 
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
 
+        //top 5 episodios
+        System.out.println("Top 5 Episodios");
 
+        datosEpisodios.stream()
+                .filter(e-> !e.evaluacion().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .limit(5)
+                .forEach(System.out::println);
 
+        //convirteiendo los datso a una lista del tipo epísodio
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d-> new Episodio(t.numero() ,d)))
 
-
-
-
-
-
-
-
-
-
+                .collect(Collectors.toList());
 
 
 
